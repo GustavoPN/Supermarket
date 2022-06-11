c056ea746b0b8a37b17aabaf01bab22eb6ddca98
GUSTAVO PIMENTEL NEGREIROS <GNEGREIROSPIMENTEL@GMAIL.COM>
2022-06-11 16:13:54 -0300

    Adicionando arquivos do projeto

---
+++ SocialMiner.SupermarketProducts.Domain/Core/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialMiner.SupermarketProducts.Domain.Core
+{
+    public abstract class Entity : Notifiable
+    {
+
+        public Guid Id { get; protected set; }
+
+        public virtual bool IsValid()
+        {
+            return !this.Errors.Any();
+        }
+    }
+
+}
---
+++ SocialMiner.SupermarketProducts.Repositores/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+using SocialMiner.SupermarketProducts.Core.Repository;
+using SocialMiner.SupermarketProducts.Core.Settings;
+using SocialMiner.SupermarketProducts.Domain.Product;
+
+namespace SocialMiner.SupermarketProducts.Repositores
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MongoDbSettings _settings;
+        private readonly IMongoCollection<Product> _collection;
+       
+        public ProductRepository(MongoDbSettings settings)
+        {
+            _settings = settings;
+            var database = new MongoClient(settings.ConnectionString).GetDatabase(settings.DatabaseName);
+            _collection = database.GetCollection<Product>("product");
+        }
+
+        public async Task AddAsync(Product document)
+        {
+            await _collection
+                    .ReplaceOneAsync(x => x.Id == document.Id,
+                                       document,
+                                       new ReplaceOptions { IsUpsert = true });
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var result = await _collection.DeleteOneAsync<Product>(x => x.Id == id);
+
+            return result.DeletedCount > 0;
+        }
+
+        public async Task<IEnumerable<Product>> GetAsync()
+        {
+            return (await _collection.FindAsync<Product>(x => x.Id != Guid.Empty)).ToList();
+        }
+
+        public async Task<Product> GetAsync(Guid id)
+        {
+            return (await _collection.FindAsync<Product>(x => x.Id != id)).First();
+        }
+    }
+}
