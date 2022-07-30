b8e63c8dd3926ae51a2e9206b16b2b60b6e5b6b9
GUSTAVO PIMENTEL NEGREIROS <GNEGREIROSPIMENTEL@GMAIL.COM>
2022-07-30 16:40:05 -0300

    Added post product feature

--- SocialMiner.SupermarketProducts.UseCases/CatalogUseCases/GetProductsById/GetProductsByIdRequest.cs
+++ SocialMiner.SupermarketProducts.UseCases/CatalogUseCases/GetProductsById/GetProductsByIdRequest.cs
@@ -5,6 +5,13 @@ namespace SupermarketProducts.UseCases.CatalogUseCases.GetProductsById
 {
     public class GetProductsByIdRequest : IRequest<ApiResponse<GetProductsByIdResponse>>
     {
+        private object id;
+
+        public GetProductsByIdRequest(object id)
+        {
+            this.id = id;
+        }
+
         public Guid Id { get; set; }
 
     }
